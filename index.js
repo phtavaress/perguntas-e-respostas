1d7a148714b1b13c75755b4720dcf8a76d1482fc
Pedro <ph_tavares@hotmail.com>
2024-08-08 14:02:55 -0300

    Configurando e exibindo HTML com EJS

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const ejs = require('ejs');
+
+
+const app = express();
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(8080, () => {console.log('App rodando')});
