c779a5ada22ed8b01450e1c3be8389775a6d7f49
Pedro <ph_tavares@hotmail.com>
2024-08-10 15:29:10 -0300

    Instalando o Bootstrap

--- index.js
+++ index.js
@@ -4,28 +4,13 @@ const ejs = require('ejs');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 
-app.get('/:nome/:lang', (req, res) => {
-    
-    const nome = req.params.nome;
-    const lang = req.params.lang;
-    const exibirMsg = false;
-    const produtos = [
-        {nome: 'Doritos', preco: 3.14},
-        {nome: 'Coca-Cola', preco: 5},
-        {nome: 'Leite', preco: 1.45}
-    ]
+app.get('/', (req, res) => {
 
 
-    res.render('index', {
-        nome: nome,
-        lang: lang,
-        empresa: 'Fantasy',
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+    res.render('index');
 })
 
 app.listen(8080, () => {console.log('App rodando')});
