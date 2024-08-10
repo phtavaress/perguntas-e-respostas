f0009adf69e4aa7e43261f004baba8b0b4f0a559
Pedro <ph_tavares@hotmail.com>
2024-08-10 09:20:47 -0300

    Estruturas de repetição com EJS

--- index.js
+++ index.js
@@ -7,13 +7,24 @@ app.set('view engine', 'ejs');
 
 
 app.get('/:nome/:lang', (req, res) => {
+    
     const nome = req.params.nome;
     const lang = req.params.lang;
+    const exibirMsg = false;
+    const produtos = [
+        {nome: 'Doritos', preco: 3.14},
+        {nome: 'Coca-Cola', preco: 5},
+        {nome: 'Leite', preco: 1.45}
+    ]
+
+
     res.render('index', {
         nome: nome,
         lang: lang,
         empresa: 'Fantasy',
-        inscritos: 8000
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos: produtos
     });
 })
 
