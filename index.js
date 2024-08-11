6b4bbd17c32ce71c96928376e474c836d2d7e549
Pedro <ph_tavares@hotmail.com>
2024-08-11 15:00:53 -0300

    Criando formulário de perguntas

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-
-
     res.render('index');
-})
+});
+
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar');
+});
 
 app.listen(8080, () => {console.log('App rodando')});
